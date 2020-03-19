6fc5948b0b92b17d3000fac0a656e50ea56c7bf3
Michael Bugaevski <mikejm618@gmail.com>
2020-03-19 11:08:05 -0400

    mytake08: 1.1.0

--- mytake08.go
+++ mytake08.go
@@ -1,5 +1,7 @@
 package gomodtake08
 
+//Attempt to update downloaded module, using next version
+
 func MyFuncTake08(parm string) string {
 	returnedValue := parm + " - Looki"
 	return returnedValue
