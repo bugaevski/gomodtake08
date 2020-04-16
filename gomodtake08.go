b6dea6ca205d4b5b03b2afd5cd398c7320f547d6
Michael Bugaevski <mikejm618@gmail.com>
2020-04-16 11:27:35 -0400

    gomodtake08: v1.1.0

--- gomodtake08.go
+++ gomodtake08.go
@@ -1,7 +1,9 @@
 package gomodtake08
 
+
 //MyFuncTake08 first attempt to use module
 func MyFuncTake08(parm string) string {
 	returnedValue := parm + " - Looki"
 	return returnedValue
 }
+
--- gomodtake08_test.go
+++ gomodtake08_test.go
@@ -4,9 +4,11 @@ import (
 	"testing"
 )
 
+
 func TestMyTake08(t *testing.T) {
 	want := "Mikki - Looki"
 	if got := MyFuncTake08("Mikki"); got != want {
 		t.Errorf("MyFuncTake08() = %q, want %q", got, want)
 	}
 }
+
