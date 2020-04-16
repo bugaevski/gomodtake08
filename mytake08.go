d53f67901f7ee70bc06a3cc3e844f0982927bc27
Michael Bugaevski <mikejm618@gmail.com>
2020-04-16 11:10:50 -0400

    Delete mytake08.go

--- mytake08.go
+++
@@ -1,8 +0,0 @@
-package gomodtake08
-
-//Attempt to update downloaded module, using next version
-
-func MyFuncTake08(parm string) string {
-	returnedValue := parm + " - Looki"
-	return returnedValue
-}
